65e6c1ffffe1277c93867e8fe3b0b0b3aab5402f
weioon <weioon0509@gmail.com>
2025-05-11 21:53:03 +0800

    Enhance generate-cert script: add error handling and command tracing for improved debugging

--- frontend/generate-cert.sh
+++ frontend/generate-cert.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+set -e # Exit immediately if a command exits with a non-zero status.
+set -x # Print commands and their arguments as they are executed.
 
 # Create a directory for the certificates if it doesn't exist
 mkdir -p /etc/nginx/ssl
