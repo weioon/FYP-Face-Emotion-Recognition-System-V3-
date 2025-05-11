673dcc32a35617607e6401cb53fdbe1a0ce157f6
weioon <weioon0509@gmail.com>
2025-05-11 22:01:31 +0800

    Revert "Enhance generate-cert script: add error handling and command tracing for improved debugging"
    This reverts commit 65e6c1ffffe1277c93867e8fe3b0b0b3aab5402f.

--- frontend/generate-cert.sh
+++ frontend/generate-cert.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-set -e # Exit immediately if a command exits with a non-zero status.
-set -x # Print commands and their arguments as they are executed.
 
 # Create a directory for the certificates if it doesn't exist
 mkdir -p /etc/nginx/ssl
